921e8e4504d30b71c8e5d43262304208b17556ce
Aiyan-Faras <farasaiyan@gmail.com>
2025-09-28 18:33:40 +0530

    Implemented RabbitMQ Bus

--- MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -1,8 +1,11 @@
-﻿using MediatR;
+﻿using System.Text;
+using MediatR;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Domain.Core.Commands;
 using MicroRabbit.Domain.Core.Events;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace MicroRabbit.Infra.Bus
 {
@@ -25,15 +28,96 @@ namespace MicroRabbit.Infra.Bus
         public void Publish<T>(T @event) where T : Event
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            
-            
-            
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+                channel.QueueDeclare(eventName, false, false, false, null);
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", eventName, false, null, body);
+            }         
         }
         public void Subscribe<T, Th>()
             where T : Event
             where Th : IEventHandler<T>
         {
-            throw new NotImplementedException();
+            var eventName = typeof(T).Name;
+            var handlertype = typeof(Th);
+
+            if (!_eventTypes.Contains(typeof(T))) 
+            {
+                _eventTypes.Add(typeof(T));
+            }
+
+            if (!_handlers.ContainsKey(eventName))
+            {
+                _handlers.Add(eventName, new List<Type>()); 
+            }
+
+            if (_handlers[eventName].Any(x => x.GetType() == handlertype))
+            {
+                throw new ArgumentException($"Handler type {handlertype.Name} is already registered for '{eventName}' ", nameof(handlertype));
+            }
+
+            _handlers[eventName].Add(handlertype);
+            StartBasicConsume<T>();
+        }
+
+        private void StartBasicConsume<T>() where T : Event
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                DispatchConsumersAsync = true
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var eventName = typeof(T).Name;
+            channel.QueueDeclare(eventName, false, false, false, null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume(eventName, true, consumer);
+
+
+        }
+
+        private async Task Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var eventName = e.RoutingKey;
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            try
+            {
+                await ProcessEvent(eventName, message).ConfigureAwait(false); 
+            }
+            catch (Exception ex) 
+            {
+            }
+
+
+        }
+
+        private async Task ProcessEvent(string eventName, string message)
+        {
+            if (_handlers.ContainsKey(eventName))
+            { 
+                var subscriptions = _handlers[eventName];
+                foreach (var subscription in subscriptions)
+                {
+                    var handler = Activator.CreateInstance(subscription);
+                    if (handler == null) continue;
+                    var eventType = _eventTypes.SingleOrDefault(x => x.Name == eventName);
+                    var @event = JsonConvert.DeserializeObject(message, eventType);
+                    var concretetype = typeof(IEventHandler<>).MakeGenericType(eventType);
+                    await (Task)concretetype.GetMethod("Handle").Invoke(handler, new object[] { @event });
+                }
+            }
         }
     }
 }
